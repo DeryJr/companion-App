9015424b0933512fd11b849b1bd2d0f983cc2029
IvanMijic0 <ivanmijic159951@gmail.com>
2023-05-27 18:54:50 +0200

    Signup Design almost Done

--- app/src/main/java/com/example/companion/SignupScreen.kt
+++ app/src/main/java/com/example/companion/SignupScreen.kt
@@ -4,8 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,12 +55,18 @@ fun SignupContent(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp)
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         SignupTopBar()
         Spacer(modifier = Modifier.padding(top = 80.dp))
         SignupText()
         Spacer(modifier = Modifier.weight(1f))
+        Email()
+        Password()
+        Spacer(modifier = Modifier.weight(1f))
+        SignupButton()
+
     }
 }
 
@@ -91,7 +110,8 @@ fun SignupText() {
         modifier = Modifier
             .clip(RoundedCornerShape(16.dp))
             .background(Color(0x8CFAB1B1))
-            .padding(horizontal = 8.dp)
+            .padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.Start
     ) {
         Text(
             text = buildAnnotatedString {
@@ -115,6 +135,115 @@ fun SignupText() {
                 }
             },
             textAlign = TextAlign.Start,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Email() {
+
+    val emailState = remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = emailState.value,
+        onValueChange = { emailState.value = it },
+        label = { Text(text = "email", color = Color(0xFF3C0101), fontWeight = FontWeight.Bold) },
+        placeholder = { Text(text = "smt@stu.ibu.edu.ba", color = Color(0xFF3C0101)) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            containerColor = Color(0xFFFFB1B1),
+            textColor = Color(0xFF3C0101),
+            cursorColor = Color(0xFF3C0101),
+            unfocusedBorderColor = Color(0xFF521616),
+            focusedBorderColor = Color(0xFF3C0101),
+            errorBorderColor = Color.Red,
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Password() {
+
+    val passwordState = remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = passwordState.value,
+        onValueChange = { passwordState.value = it },
+        label = {
+            Text(
+                text = "password",
+                color = Color(0xFF3C0101),
+                fontWeight = FontWeight.Bold
+            )
+        },
+        placeholder = { Text(text = "******", color = Color(0xFF3C0101)) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            containerColor = Color(0xFFFFB1B1),
+            textColor = Color(0xFF3C0101),
+            cursorColor = Color(0xFF3C0101),
+            unfocusedBorderColor = Color(0xFF521616),
+            focusedBorderColor = Color(0xFF3C0101),
+            errorBorderColor = Color.Red
+        ),
+    )
+}
+
+@Composable
+fun SignupButton() {
+    Button(
+        onClick = { },
+        modifier = Modifier.fillMaxWidth(.6f),
+        colors = ButtonDefaults.buttonColors(Color(0xFFFFB1B1))
+    ) {
+        Text(
+            text = "Sign Up",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF3C0101)
+        )
+    }
+    Text(text = "or", color = Color(0xFFFFB1B1))
+    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color(0xFF682B2B))
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_google),
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color(0xFF682B2B))
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_meta),
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+    }
+    Text(
+        text = "Already have an account?",
+        color = Color(0xFFFFB1B1),
+        modifier = Modifier.padding(top = 16.dp)
+    )
+    TextButton(onClick = { /*TODO*/ }, modifier = Modifier) {
+        Text(
+            text = "Login",
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFFFB1B1),
+            fontSize = 20.sp
         )
     }
 }
